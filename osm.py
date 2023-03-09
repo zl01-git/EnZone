4b3ab4b5523281a328b0f49abeed533b289ecb11
zl01-git <as55rus@gmail.com>
2023-03-09 13:43:11 +0300

    update

--- osm.py
+++ osm.py
@@ -1,12 +1,25 @@
 import folium
-from zones import MarkerCoordinates, markers
+from zones import Marker, markers, circuts
+from geopy import distance
 
 
-omsk = MarkerCoordinates("omsk", "omsk", 54.991416, 73.364253)
 
-m = folium.Map(location=[omsk.lat, omsk.long], zoom_start=10)
+omsk = Marker("omsk", "omsk", 54.991416, 73.364253)
+
+m = folium.Map(location=[omsk.lat, omsk.long], zoom_start=11)
+
+
+
+folium.Circle(location=[circuts[1].lat, circuts[1].long], 
+radius=circuts[1].radius, tooltip=circuts[1].name, popup=circuts[1].name).add_to(m)
 
 for marker in markers:
-    folium.Marker(location=[marker.lat, marker.long], tooltip=marker.name, popup="{}".format(marker.descriptions) ).add_to(m)
+    dist = distance.geodesic([circuts[1].lat, circuts[1].long], 
+    [marker.lat, marker.long]).km * 1000
+    if dist < circuts[1].radius:
+        folium.Marker(location=[marker.lat, marker.long], 
+        tooltip=marker.name, popup=marker.descriptions).add_to(m)
+
+
 
 m.save("map.html")
